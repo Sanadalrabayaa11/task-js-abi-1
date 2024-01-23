e520edbf2e36f39bad83b554e1a5bc540db7b0b5
sanadalrabayaa11 <sanadhayajneh11@gmail.com>
2024-01-23 23:11:50 +0300

    first commit

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-async function goods(){
+async function goods() {
     
-    const response = await fetch("https://dummyjson.com/products");
-    const data = await response.json();
-    const products = data.products
+  const response = await fetch("https://dummyjson.com/products");
+  const data = await response.json();
+  const products = data.products;
 
-    const result = products.map(function(ele){
-    return`
-    <div class="products">
-    <h2>${ele.title}</h2>
-    <img src = "${ele.thumbnail}"/>
-    <span>${ele.price}</span>
-    <span>${ele. discountPercentage}</span>
-    <span>${ele. rating}</span>
-    <span>${ele.stock}</span>
-   </div>
-    `;
-    }).join("");
-    document.querySelector(".products").innerHTML=result;
-    }
-    goods();
- 
+  const result = products
+    .map(function (ele) {
+      return `
+        <div class="Prodct">
+            <h3>${ele.title}</h3>
+            <img src="${ele.images[0]}" alt="">
+            <p>${ele.rating}</p>
+            <span>$ ${ele.price}</span>
+            
+        </div>
+           `;
+    })
+    .join(" ");
+  document.querySelector(".products").innerHTML = result;
+}
+
+goods();
